c2f211853f5fbd6027c50d4d7e4ffbb42845bb62
Tibor Udvari <tibor.udvari@icloud.com>
2025-02-05 14:44:09 +0100

    chore: README update and sample data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
+
