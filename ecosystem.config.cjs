f4826e0320c73a5989d9db334f1b9b78351de252
Tibor Udvari <tibor.udvari@icloud.com>
2025-02-05 20:49:31 +0100

    feat: Starter content

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,12 +5,16 @@ module.exports = {
       script: "npm",
       args: "run dev:frontend",
       stop_signal: "SIGINT",
+      wait_ready: true,
+      autorestart: true,
     },
     {
       name: "backend",
       script: "npm",
       args: "run dev:backend",
       stop_signal: "SIGINT",
+      wait_ready: true,
+      autorestart: true,
     },
   ]
 };
