6425a3846f3327c5febb9c25b4ba2ede08c1a87e
Tibor Udvari <tibor.udvari@icloud.com>
2025-02-05 14:55:00 +0100

    chore: Github Pages deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://localhost2025.metaa.ch",
   i18n: {
     locales: ["en", "fr"],
     defaultLocale: "en",
